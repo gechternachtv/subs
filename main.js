05d1694b41a2f295dc61b736f333192d7b002fa0
gechternacht <gabriel@agenciavinci.com.br>
2025-08-12 23:48:08 -0300

    srt removed

--- main.js
+++ main.js
@@ -1,35 +1,33 @@
-
-
 //vid sub cytube
 
 
 const container = document.querySelector('#leftcontrols');
 const input = document.createElement('input');
 input.type = 'text';
-input.placeholder = 'subtitle link here (only works with raw video)';
+input.placeholder = 'subtitle link (.vtt only, on rawvideo)';
 
 const button = document.createElement('button');
-button.textContent = 'Send';
+button.textContent = 'Add';
 
 container.appendChild(input);
 container.appendChild(button);
 
-// Add event listener to button
 button.addEventListener('click', () => {
 
 	try{
   const player = videojs(document.querySelector("video").id); 
-
+const src = input.value
 player.addRemoteTextTrack({
   kind: 'subtitles',
-  src: input.value,  // VTT FILE, NO CORS
+  src: src,  // VTT FILE, NO CORS
   srclang: 'en',
   label: 'custom subs',
   default: true
 }, false);
-
+input.value = ""
 }catch(err){
 		console.warn(err)
+input.value = err
 	}
 });
 
